0b8aa3c9b08557ef5f4d5be68a05e6183e07b039
Mayara-tech <67807684+Mayara-tech@users.noreply.github.com>
2022-08-04 22:57:23 -0300

    Leitura dos arquivos
    - Leitura e manipulação do arquivo filtrado

---
+++ Avaliacao Spotify.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+
+    FILE *text1;
+    char data[20], hora[10], letra;
+    int nota;
+
+    text1 = fopen("spotify.csv", "r");
+
+    if(text1 == NULL){
+        printf("Erro!");
+        system("pause");
+        exit(0);
+    }
+    fscanf (text1, "%c", &letra);
+    print("%c", letra);
+
+  /*  do{
+        fscanf (text1, "%c", &letra);
+        print("%c", letra);
+    }while (letra != '\n');
+    */
+
+    //  fscanf (text1, "%s", data);
+    //fgets(data, 100, text1);
+    //printf("Titulos: %s", data);
+
+    while( fscanf (text1, "%s %s %d", data, hora, &nota) !=  EOF){
+
+            printf("Data:%s Hora:%s Nota:%d\n", data, hora, &nota);
+    }
+
+    system("pause");
+    fclose(text1);
+
+}
