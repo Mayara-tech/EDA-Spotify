6a25ec06272f552f1ae59909c03a4ac28781d9ff
Mayara-tech <67807684+Mayara-tech@users.noreply.github.com>
2022-08-16 00:41:26 -0300

    Update Avaliacao Spotify.c

--- Avaliacao Spotify.c
+++ Avaliacao Spotify.c
@@ -11,12 +11,12 @@ Arthur D'Assumpção */
 #include <stdlib.h>     //BIBLIOTECAS
 #include <string.h>
 
+// CRIANDO STRUCT
 struct AvaliacaoSpotify
 {
     int nota;
     char ano[8];
 };
-
 typedef struct AvaliacaoSpotify avaliacao;
 
 
@@ -24,7 +24,7 @@ int main(){
     
 
 //DECLARANDO AS VARIAVEIS
-    FILE *arquivo1,*arquivo3;
+    FILE *arquivo1,*arquivo2,*arquivo3;
     char ano[8],dia[4],hora[15],letra,nota;
     avaliacao janeiro [4000];
     int i;
@@ -32,17 +32,14 @@ int main(){
     char avaliacao_ano[8];
     int avaliacao_nota;
 
-    for(i=0;i<4000;i++)
-    
-
 
 //ABRINDO OS ARQUIVOS    
     arquivo1 = fopen("Planilha Dados Filtrados.csv", "r");
- //   arquivo2 = fopen("Dados.csv","w");
+    arquivo2 = fopen("Dados.csv","w");
     arquivo3 = fopen("AvaliacaoJaneiro.csv","w");
 
 //VERIFICANDO SE OS ARQUIVOS FORAM ABERTOS CORRETAMENTE
-    if(arquivo1 == NULL ){
+    if(arquivo1 == NULL && arquivo2 == NULL){
         printf("Não foi possivel abrir arquivo!");
         system("pause");
         exit(0);
@@ -50,9 +47,10 @@ int main(){
 //COPIANDO OS DADOS NECESSARIOS DA PLANILHA DE DADOS FILTRADOS PARA PLANILHA COPIA
     do{
         fscanf (arquivo1, "%c", &letra);
- //       fprintf(arquivo2,"%c", letra);
+        fprintf(arquivo2,"%c", letra);
     }while (letra != '\n');
 
+// ESTRUTURA TESTE PARA SEPARAÇÃO POR MES
     while( fscanf (arquivo1, "%7s %3s %9s %c", ano,dia,hora,&nota) !=  EOF){
         strcpy(janeiro[contador].ano, ano);
         janeiro[contador].nota = nota;
@@ -61,22 +59,20 @@ int main(){
             janeiro[4000].nota = avaliacao_nota;
         }
     }
-
         contador++;
     }
   system("pause");
 
-
-
 //FECHANDO ARQUIVOS 
     fclose(arquivo1);
-//    fclose(arquivo2);
+    fclose(arquivo2);
     fclose(arquivo3);
 
-printf("resultados: \n");
-for(i=0; i<4000; i++)
-  if(janeiro[i].ano == "01-2022"){
-    printf("%s %d", janeiro[i].ano,janeiro[i].nota);
+// TESTE PARA MOSTRAR RESULTADOS 
+    printf("resultados: \n");
+    for(i=0; i<4000; i++)
+    if(janeiro[i].ano == "01-2022"){
+        printf("%s %d", janeiro[i].ano,janeiro[i].nota);
   }
 
     return 0;
