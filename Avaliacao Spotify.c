0243b2d2ee89d61cae62aac26ad36122199ca9bf
Mayara-tech <67807684+Mayara-tech@users.noreply.github.com>
2022-08-19 22:23:41 -0300

    Update Avaliacao Spotify.c

--- Avaliacao Spotify.c
+++ Avaliacao Spotify.c
@@ -1,79 +1,217 @@
 /* UNIVERSIDADE DE BRASILIA - FGA 
 GRUPO G- SPOTIFY/SelectionSort - EDA
-Alunos: Mayara Alves de Oliveira
+ALUNOS:
 Vinicius de Oliveira Santos
+Mayara Alves de Oliveira
 Dara Maria Barbosa de Sousa
 Paulo Vinicius Pinheiro do Nascimento
 Arthur D'Assumpção */
 
-
 #include <stdio.h>
-#include <stdlib.h>     //BIBLIOTECAS
-#include <string.h>
+#include <stdlib.h>    //BIBLIOTECAS
+#include <time.h>
 
-// CRIANDO STRUCT
-struct AvaliacaoSpotify
-{
-    int nota;
-    char ano[8];
-};
-typedef struct AvaliacaoSpotify avaliacao;
-
+//FUNÇÕES
+void selectionSort(int vet[], int tam);
+int smallerIndex(int vet[], int tam, int ini);
 
 int main(){
-    
 
-//DECLARANDO AS VARIAVEIS
-    FILE *arquivo1,*arquivo2,*arquivo3;
-    char ano[8],dia[4],hora[15],letra,nota;
-    avaliacao janeiro [4000];
-    int i;
-    int contador = 0;
-    char avaliacao_ano[8];
-    int avaliacao_nota;
+// DEFININDO VARIAVEIS
+
+    FILE *text1,*text2,*text3;
+    char ano[8],dia[4],hora[15],letra,hifen;
+    int c1[5231],c2[7458],c3[8023],c4[13803],c5[10922],c6[12257],c7[3900];
+    int i,j,posic,nota,mes;
+    float c[]={0,0,0,0,0,0,0};
+    float soma[]={0,0,0,0,0,0,0};
+    float media[]={0,0,0,0,0,0,0};
+    float mediames[]={0,0,0,0,0,0,0};
+    clock_t start_t,end_t;
+    double final_t;
+    start_t = clock();
 
 
-//ABRINDO OS ARQUIVOS    
-    arquivo1 = fopen("Planilha Dados Filtrados.csv", "r");
-    arquivo2 = fopen("Dados.csv","w");
-    arquivo3 = fopen("AvaliacaoJaneiro.csv","w");
+//ABRINDO OS ARQUIVOS   
+    text1 = fopen("Planilha Dados Filtrados.csv", "r");
+    text2 = fopen("Dados_ordenados.csv","w");
+    text3 = fopen("Dados_grafico.csv","w");
 
 //VERIFICANDO SE OS ARQUIVOS FORAM ABERTOS CORRETAMENTE
-    if(arquivo1 == NULL && arquivo2 == NULL){
-        printf("Não foi possivel abrir arquivo!");
-        system("pause");
+
+    if(text1 == NULL){
+        printf("Erro ao abrir o texto1");
         exit(0);
     }
-//COPIANDO OS DADOS NECESSARIOS DA PLANILHA DE DADOS FILTRADOS PARA PLANILHA COPIA
-    do{
-        fscanf (arquivo1, "%c", &letra);
-        fprintf(arquivo2,"%c", letra);
-    }while (letra != '\n');
+    if(text2 == NULL){
+        printf("Erro ao abrir o texto2");
+        exit(0);
+    }
+    if(text3 == NULL){
+        printf("Erro ao abrir o texto3");
+        exit(0);
+    }
 
-// ESTRUTURA TESTE PARA SEPARAÇÃO POR MES
-    while( fscanf (arquivo1, "%7s %3s %9s %c", ano,dia,hora,&nota) !=  EOF){
-        strcpy(janeiro[contador].ano, ano);
-        janeiro[contador].nota = nota;
-    while(fscanf(arquivo3, "%s %d", avaliacao_ano, avaliacao_nota )!= EOF) {
-        if(avaliacao_ano == "01-20022"){
-            janeiro[4000].nota = avaliacao_nota;
+    do{
+        fscanf (text1, "%c", &letra);
+        fprintf(text2,"%c", letra);
+    }while (letra != '\n');
+   
+
+//CALCULANDO A MEDIA DAS AVALIAÇÕES MENSAIS
+    while( fscanf (text1, "%4s %c %d %3s %9s %d", ano,&hifen,&mes,dia,hora,&nota) !=  EOF){
+        if(mes == 7){
+            posic=c[mes-1];
+            c7[posic] = nota;
+            soma[mes-1] = soma[mes-1] + nota;
+            c[mes-1]++;
+        }
+        if(mes == 6){
+            posic=c[mes-1];
+            c6[posic] = nota;
+            soma[mes-1] = soma[mes-1] + nota;
+            c[mes-1]++;
+        }
+        if(mes == 5){
+            posic=c[mes-1];
+            c5[posic] = nota;
+
+            soma[mes-1] = soma[mes-1] + nota;
+
+            c[mes-1]++;
+        }
+        if(mes == 4){
+            posic=c[mes-1];
+            c4[posic] = nota;
+
+            soma[mes-1] = soma[mes-1] + nota;
+
+            c[mes-1]++;
+        }
+        if(mes == 3){
+            posic=c[mes-1];
+            c3[posic] = nota;
+
+            soma[mes-1] = soma[mes-1] + nota;
+
+            c[mes-1]++;
+        }
+        if(mes == 2){
+            posic=c[mes-1];
+            c2[posic] = nota;
+
+            soma[mes-1] = soma[mes-1] + nota;
+
+            c[mes-1]++;
+        }
+        if(mes == 1){
+            posic=c[mes-1];
+            c1[posic] = nota;
+            soma[mes-1] = soma[mes-1] + nota;
+            c[mes-1]++;
         }
     }
-        contador++;
+    for(i=0;i<7;i++){
+        media[i] = soma[i]/c[i];
     }
-  system("pause");
+    for(i=0;i<7;i++){
+        mediames[i] = media[i];
+    }
 
+// APLICANDO O SELECTIONSORT PARA ORDENAR AS NOTAS DE CADA MES
+    selectionSort(c7,c[6]);
+    selectionSort(c6,c[5]);
+    selectionSort(c5,c[4]);
+    selectionSort(c4,c[3]);
+    selectionSort(c3,c[2]);
+    selectionSort(c2,c[1]);
+    selectionSort(c1,c[0]);
+
+// APLICANDO O SELECTIONSORT PARA ORDENAR A MEDIA MENSAL DAS AVALIAÇÕES
+    selectionSort(media,7);
+
+//COPIANDO E ORGANIZANDO OS DADOS DE ACORDO COM O MES NO ARQUIVO DE DADOS ORDENADOS  
+
+    for(i=0;i<c[6];i++){
+        fprintf(text2,"mes7: %d\n",c7[i]);
+    }
+    for(i=0;i<c[5];i++){
+        fprintf(text2,"mes6: %d\n",c6[i]);
+    }
+    for(i=0;i<c[4];i++){
+        fprintf(text2,"mes5: %d\n",c5[i]);
+    }
+    for(i=0;i<c[3];i++){
+        fprintf(text2,"mes4: %d\n",c4[i]);
+    }
+    for(i=0;i<c[2];i++){
+        fprintf(text2,"mes3: %d\n",c3[i]);
+    }
+    for(i=0;i<c[1];i++){
+        fprintf(text2,"mes2: %d\n",c2[i]);
+    }
+    for(i=0;i<c[0];i++){
+        fprintf(text2,"mes1: %d\n",c1[i]);
+    }
+    
+    fprintf(text3,"Time_submitted,Avaliacao_Media\n");
+    
+//COPIANDO AS MEDIAS MENSAIS ORDENADAS PARA O ARQUIVO DE DADOS GRAFICOS  
+
+    for(i=0;i<7;i++){
+        if(mediames[0] == media[i]){
+            fprintf(text3,"2022_01,%.2f\n",media[i]);
+        }
+        if(mediames[1] == media[i]){
+            fprintf(text3,"2022_02,%.2f\n",media[i]);
+        }
+        if(mediames[2] == media[i]){
+            fprintf(text3,"2022_03,%.2f\n",media[i]);
+        }
+        if(mediames[3] == media[i]){
+            fprintf(text3,"2022_04,%.2f\n",media[i]);
+        }
+        if(mediames[4] == media[i]){
+            fprintf(text3,"2022_05,%.2f\n",media[i]);
+        }
+        if(mediames[5] == media[i]){
+            fprintf(text3,"2022_06,%.2f\n",media[i]);
+        }
+        if(mediames[6] == media[i]){
+            fprintf(text3,"2022_07,%.2f\n",media[i]);
+        }
+    }
 //FECHANDO ARQUIVOS 
-    fclose(arquivo1);
-    fclose(arquivo2);
-    fclose(arquivo3);
+    fclose(text1);
+    fclose(text2);
 
-// TESTE PARA MOSTRAR RESULTADOS 
-    printf("resultados: \n");
-    for(i=0; i<4000; i++)
-    if(janeiro[i].ano == "01-2022"){
-        printf("%s %d", janeiro[i].ano,janeiro[i].nota);
-  }
+//FUNÇAO PARA MEDIR O TEMPO DE PROCESSAMENTO DO PROGRAMA 
+
+    end_t = clock();
+    final_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("Tempo de processamento do programa: %f\n",final_t);
 
     return 0;
+}
+//FUNÇÃO DE ORDENAÇÃO SELECTIONSORT
+
+void selectionSort(int vet[], int tam){
+    int i, min, aux , j;
+    for(i=0; i<tam; i++){
+        //Acha posicao do menor elemento a partir de i:
+        min = smallerIndex(vet, tam, i);
+        aux = vet[i];
+        vet[i] = vet[min];
+        vet[min] = aux;
+    }
+}
+//FUNÇÃO PARA ACHAR MENOR POSIÇÃO
+int smallerIndex(int vet[], int tam, int ini){
+    int min = ini, j;
+    for(j=ini+1; j<tam; j++){
+        if(vet[min] > vet[j]){
+            min = j;
+        }
+    }
+    return min;
 }
